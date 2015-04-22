6ee7e40d4d9a99717fb713e5544aafd43a301ead
looloosha <a.davidyuk@yahoo.com>
2015-04-21 23:04:17 -0500

    This time I actually worked on the structure.

---
+++ src/org/vanillaplus/statfacts/managers/SettingsManager.java
@@ -0,0 +1,13 @@
+package org.vanillaplus.statfacts.managers;
+
+/**
+ * 
+ * @author Alex Davidyuk
+ * This is where the settings manager will go.
+ * Everyone should learn how a SettingsManager works.
+ * Go here to learn: https://www.youtube.com/watch?v=_S0hIVT9wAA&list=PLAF3anQEEkzQXwse2G7QXJELQPYe7numC&index=3
+ * 
+ */
+public class SettingsManager {
+	
+}
